26e26fb8582f994ee5e60f6f09a455c538860234
doododae <kevin.loo69@gmail.com>
2026-09-09 18:40:19 -0400

    quant details update
    updated getDetails to incorporate all dbs now

--- quanDetails.R
+++ quanDetails.R
@@ -1,5 +1,13 @@
 getDetails <- function(structure, data) {
-  outp <- filter(data, name == structure) |>
-          select(name, floating_Na, floating_NH3, charge, Exep.isotopic.mz, mono_mw, time, abundance, score)
+  if(hasName(data, "floating_Na")) {
+    outp <- filter(data, name == structure) |>
+      select(name, floating_Na, floating_NH3, charge, Exep.isotopic.mz, mono_mw, time, abundance, score)
+  } else if(hasName(data, "floating_Mn")) {
+    outp <- filter(data, name == structure) |>
+      select(name, floating_Mn, floating_NH3, charge, Exep.isotopic.mz, mono_mw, time, abundance, score)
+  } else {
+    outp <- filter(data, name == structure) |>
+      select(name, floating_NH3, charge, Exep.isotopic.mz, mono_mw, time, abundance, score)
+  }
   return(outp)
 }
